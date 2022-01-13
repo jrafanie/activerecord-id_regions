2972600b0e9acd85fa8e760bb76652d469a756b9
Joe Rafaniello <jrafanie@gmail.com>
2022-01-13 16:05:13 -0500

    Support ruby 2.6, 2.7, and 3.0 in the only way possible.
    https://eregon.me/blog/2021/02/13/correct-delegation-in-ruby-2-27-3.html

--- lib/active_record/id_regions/migration.rb
+++ lib/active_record/id_regions/migration.rb
@@ -2,7 +2,7 @@ module ActiveRecord::IdRegions
   module Migration
     ALLOWED_ID_VALUES = [false, :uuid, "uuid"].freeze
 
-    def create_table(table_name, **options)
+    def create_table(table_name, options = {})
       options[:id] = :bigserial unless ALLOWED_ID_VALUES.include?(options[:id])
       value = anonymous_class_with_id_regions.rails_sequence_start
       super
@@ -10,6 +10,7 @@ module ActiveRecord::IdRegions
 
       set_pk_sequence!(table_name, value) unless value == 0
     end
+    ruby2_keywords :create_table if respond_to?(:ruby2_keywords, true)
 
     def anonymous_class_with_id_regions
       ActiveRecord::IdRegions::Migration.anonymous_class_with_id_regions
